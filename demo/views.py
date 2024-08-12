9ba6c061362aeebc6405f8ce4bbfd545965df609
xxxibgdrgn <guolin1106@126.com>
2024-08-12 16:06:38 +0800

    Update views.py

--- demo/views.py
+++ demo/views.py
@@ -2,7 +2,5 @@ from django.http import JsonResponse
 
 
 def hello(request):
-    # 创建一个字典，包含你想要返回的数据
     data = {'domain1': 'www.eastbuy.com','domain2':'app.eastbuy.com'}
-    # 使用JsonResponse返回这个字典作为JSON格式的响应
     return JsonResponse(data)
